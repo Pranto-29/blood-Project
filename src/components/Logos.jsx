96a3f3af691a6e9e407980f8e14f0dba1bae481e
Pranto-29 <23pranto729@gmail.com>
2026-05-15 11:39:51 +0600

    added an message

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,19 +1,3 @@
-// import React from 'react';
-// // import logo from  '../assets/logo.png';
-// import logo from '../assets/banner/download.png';
-// import { Link } from 'react-router';
-// const Logos = () => {
-//     return (
-//       <Link to="/">
-//         <div className='flex items-end'>
-//             <img src={logo} alt="" />
-//             <h3 className='text-3xl -ms-2.5'>Zapshift</h3>
-//         </div>
-//       </Link>
-//     );
-// };
-
-// export default Logos;
 
 
 import React from 'react';
@@ -27,7 +11,7 @@ const Logos = () => {
         <div className='flex items-end gap-2'>
             <img src={logo} alt="Logo" className='w-10 h-10' />
             <h3 className='text-3xl -ms-2.5'>LifeDrop</h3>
-            <FaHeart className='text-red-500 text-xl' /> {/* icon add kora holo */}
+            <FaHeart className='text-red-400 text-xl' /> {/* icon add kora holo */}
         </div>
       </Link>
     );
